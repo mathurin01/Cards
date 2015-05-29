f4ed14b1056bc961f0f9e49d620d05ce8b97894d
mathurin <jpetiteau@gmail.com>
2015-05-29 23:34:12 +0200

    mise en place des premières requetes, sur la page des joueurs et des équipes

--- src/Game/CardBundle/Controller/TeamController.php
+++ src/Game/CardBundle/Controller/TeamController.php
@@ -8,8 +8,36 @@ class TeamController extends Controller
 {
     public function indexAction()
     {
-        $team = $this->getDoctrine()->getManager()->getRepository('GameCardBundle:Team')->findAll();
-
-        return $this->render('GameCardBundle:Team:index.html.twig', array('team' => $team));
+        return $this->render('GameCardBundle:Team:index.html.twig');
     }
+
+    public function showAction($id)
+    {
+        $team = $this->getDoctrine()->getManager()->getRepository('GameCardBundle:Team')->findById($id);
+        $nbLoose = $this->getDoctrine()->getManager()->getRepository('GameCardBundle:Game')->nbLoose($id);
+        $nbGame = $this->getDoctrine()->getManager()->getRepository('GameCardBundle:Game')->nbGame($id);
+        $nbWin = $this->getDoctrine()->getManager()->getRepository('GameCardBundle:Game')->nbWin($id);
+        /*
+         * A déporter dans une fonction pour effectuer le calcul
+         * Voir s'il ne serait pas plus rapide d'avoir les résultats précédents dans cette fonction...
+         * $nbPtsLoose = $this->getDoctrine()->getManager()->getRepository('GameCardBundle:Game')->nbPtsLoose($id);
+         */
+
+        return $this->render('GameCardBundle:Team:show.html.twig', array(
+            'team' => $team,
+            'nbWin' => $nbWin,
+            'nbGame' => $nbGame,
+            'nbLoose' => $nbLoose
+            /*'nbPtsLoose' => $nbPtsLoose*/
+    ));
+
+    }
+
+    public function menuAction()
+    {
+        $teams = $this->getDoctrine()->getManager()->getRepository('GameCardBundle:Team')->findAll();
+
+        return $this->render('GameCardBundle:Team:menu.html.twig', array('teams' => $teams));
+    }
+
 }
